d0f5e3e4564d4f0109d3144f199802811a5817cf
mmoghal <mmoghal29@gmail.com>
2023-05-31 19:58:46 -0400

    updated Auth0-config.js

--- auth0-config.js
+++ auth0-config.js
@@ -4,7 +4,7 @@ const authConfig = {
   authRequired: false,
   auth0Logout: true,
   secret: 'Super secret secret',
-  baseURL: process.env.BASE_URL,
+  baseURL: 'http://localhost:3001', // Replaced with your actual base URL
   clientID: 'xAmIzMi3vgs4srAQQZ6uuraRExaWfGKA',
   issuerBaseURL: 'https://dev-3kdcce2iloekw6mb.us.auth0.com'
 };
