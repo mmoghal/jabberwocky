dd86c76d1cfbf5b150a70f12c0e2b189067e3717
mmoghal <mmoghal29@gmail.com>
2023-05-31 22:23:42 -0400

    Updated auth0-config.js Update code on  frontend/views/layouts/welcome.handlebars

--- auth0-config.js
+++ auth0-config.js
@@ -1,13 +1,29 @@
 const { auth } = require('express-openid-connect');
 
-const authConfig = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'Super secret secret',
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_URL
-  };
-  
+const getClientSecret = () => {
+  if (process.env.CLIENT_SECRET) {
+    return process.env.CLIENT_SECRET;
+  }
+  return null;
+};
 
-module.exports = auth(authConfig);
+const authConfig = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'Super secret secret',
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  clientSecret: getClientSecret(),
+  routes: {
+    login: false, // Disable the default login route
+  },
+  authorizationParams: {
+    response_type: 'code id_token',
+    scope: 'openid profile',
+  },
+};
+
+const authMiddleware = getClientSecret() ? auth(authConfig) : (req, res, next) => next();
+
+module.exports = authMiddleware;
