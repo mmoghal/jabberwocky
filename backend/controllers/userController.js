f52aa0b63a09e0dda42efd43b8a6308c035c1cb7
Xandi <santosal2018@gmail.com>
2023-05-31 20:02:11 -0400

    bcrpyt for createUser and loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const { User, BookReview, ReadingList } = require('../models');
+const bcrypt = require('bcrypt');
 
 const userController = {
     getAllUsers: async (req, res) => {
@@ -49,8 +50,12 @@ const userController = {
 
     createUser: async (req, res) => {
         try {
-            const newUser = await User.create(req.body);
-            res.status(200).json(newUser);
+            const newUser = req.body;
+            // hash the password from the 'req.body' and saves it into 'newUser'
+            newUser.password = await bcrypt.hash(req.body.password, 10);
+            // creates a new user with a hashed password and saves it to the DB
+            const userData = await User.create(req.body);
+            res.status(200).json(userData);
         } catch (err) {
             res.status(400).json(err);
         }
@@ -89,6 +94,45 @@ const userController = {
             res.status(500).json(err);
         }
     },
+
+
+    loginUser: async (req, res) => {
+        try {
+            // we search the DB for a user with the provided username
+            const userData = await User.findOne({ where: { username: req.body.username } });
+            if (!userData) {
+              // checks if the username matches, if not returns a vague error message
+              res.status(404).json({ message: 'Login failed. Please try again!' });
+              return;
+            }
+            // use `bcrypt to compare the provided password and the hashed password
+            const validPassword = await bcrypt.compare(
+              req.body.password,
+              userData.password
+            );
+            // if password does not match username, returns error message
+            if (!validPassword) {
+              res.status(400).json({ message: 'Login failed. Please try again!' });
+              return;
+            }
+            // if they do match, return success message
+            res.status(200).json({ message: 'You are now logged in!' });
+          } catch (err) {
+            res.status(500).json(err);
+          }
+    },
+
+    logoutUser: async (req, res) => {
+        try {
+
+        }
+        catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
 };
 
 module.exports = userController;
+
+    
