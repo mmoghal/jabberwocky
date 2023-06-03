efe9ed1d43a49584f5c42b640d0e31cd60e4ae69
EldenBear <dontet1995@gmail.com>
2023-06-02 23:58:20 -0400

    Removed Auth0 and cleaned up links between pages of app

--- backend/routes/homePage.js
+++ backend/routes/homePage.js
@@ -1,12 +1,10 @@
-const { BookReview } = require("../models");
+const { Book } = require("../models");
 
 const router = require("express").Router();
 
-// Import Auth0 middleware
-const { requiresAuth } = require('express-openid-connect');
 
 router.get("/", async (req, res) => {
-    const reviewData = await BookReview.findAll().catch((err) => {
+    const reviewData = await Book.findAll().catch((err) => {
         res.json(err);
       });
       const reviews = reviewData.map((review) => review.get({ plain: true }));
---
+++ frontend/public/js/main.js
@@ -0,0 +1,4 @@
+const onClickCreateReview = (event) => {
+    window.location.replace("/createreview");
+};
+document.getElementById("createReviewBtn").addEventListener('click', onClickCreateReview);
