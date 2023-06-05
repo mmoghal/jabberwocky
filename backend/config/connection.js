18ae35ea490f1360421e29d0abd6acc7bfa3cd36
mmoghal <mmoghal29@gmail.com>
2023-06-05 19:26:35 -0400

    Prep for heroku deployment Updated connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,28 +1,34 @@
 const Sequelize = require('sequelize');
-const path = require('path')
+const path = require('path');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PW, 
-    {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    dialectOptions: {
-        decimalNumbers: true,
-    },
-    
-});
+let sequelize;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME, 
+        process.env.DB_USER, 
+        process.env.DB_PW, 
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+            dialectOptions: {
+                decimalNumbers: true,
+            },
+        }
+    );
+}
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 module.exports = sequelize;
